d4e1dac36dbcc822fa16922328bc999316efa8b3
tanuyadav2007 <ugf24cs171@jitnagpur.edu.in>
2026-07-31 23:14:29 +0530

    Area of Circle

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int radius;
+    cout<<"Enter Radius: ";
+    cin>>radius;
+    float area = 3.14 * radius * radius;
+    cout<<"The Area of Circle is: "<<area;
+}
