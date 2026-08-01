b75f5c58dac93164a4ae9e5623f55ae557503c44
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-08-01 14:17:59 +0530

    Added Simple Interest Program

---
+++ Basics_Of_CPP/Simple_Interest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float principle, rate, time, simple_interest;
+    cout<<"Enter Principle: ";
+    cin>>principle;
+    cout<<"Enter Rate: ";
+    cin>>rate;
+    cout<<"Enter time: ";
+    cin>>time;
+    simple_interest = (principle * rate * time)/100;
+    cout<<"The Simple Interest is: "<<simple_interest;
+
+}
