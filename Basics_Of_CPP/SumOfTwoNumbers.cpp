e465803591d401d104cbaa4fc547133aef93b6cc
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-08-01 14:37:20 +0530

    Sum of two numbers

---
+++ Basics_Of_CPP/SumOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float firstNumber, secondNumber, sum;
+
+    cout<<"Enter first number: ";
+    cin>>firstNumber;
+
+    cout<<"Enter second Number: ";
+    cin>>secondNumber;
+    
+    sum = firstNumber + secondNumber;
+
+    cout<<"The Sum Of Two Number is :"<<sum;
+}
