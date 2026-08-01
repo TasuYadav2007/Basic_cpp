f4640ef14d34adec843e2003d5975c1bed13202b
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-08-01 14:56:14 +0530

    Add program to calculate the square of a number

---
+++ Basics_Of_CPP/SquareOfNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float number, square;
+
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    square = number * number;
+
+    cout<<"The Square of the number is: "<<square;
+}
