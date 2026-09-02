ca8e580bc7c0508a4fc802facf302209522b79f2
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-09-02 23:46:37 +0530

    Add function to add two numbers

---
+++ Functions/Add_Two_Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Function
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout<<"Enter second number: ";
+    cin>>num2;
+
+    int result = add(num1, num2);
+
+    cout << "Sum = " << result << endl;
+
+    return 0;
+}
