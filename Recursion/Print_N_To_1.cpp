11750eafcf4dc48affedd04cea382a14355fadaa
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-09-10 23:21:13 +0530

    Add recursive function to print numbers from N to 1

---
+++ Recursion/Print_N_To_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int n) {
+    if (n == 0)
+        return;
+
+    cout << n << endl;
+
+    printNumbers(n - 1);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number: ";
+    cin >> n;
+
+    printNumbers(n);
+
+    return 0;
+}
