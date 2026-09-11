4e78d958371c787ea75af6add922459a4af015a3
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-09-11 21:37:25 +0530

    Add recursive function to count digits

---
+++ Recursion/Count_Digits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n) {
+    if (n == 0)
+        return 0;
+
+    return 1 + countDigits(n / 10);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number: ";
+    cin >> n;
+
+    int result = countDigits(n);
+
+    cout << "Number of digits = " << result << endl;
+
+    return 0;
+}
