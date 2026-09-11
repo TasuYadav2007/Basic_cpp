039e836f14e8692b63dc7340f0cc295ee050b9bb
TasuYadav2007 <ugf24cs173@jitnagpur.edu.in>
2026-09-11 21:22:12 +0530

    Add recursive function to find Fibonacci term

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter term number: ";
+    cin >> n;
+
+    int result = fibonacci(n);
+
+    cout << "Fibonacci term = " << result << endl;
+
+    return 0;
+}
